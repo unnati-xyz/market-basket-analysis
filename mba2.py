31be2e5403f351a084bf892bacf86508cebd1bee
bondi <nithinjoisr@gmail.com>
2016-05-25 20:08:58 +0530

    defined function required for deleting item-sets with support below min-support

--- mba2.py
+++ mba2.py
@@ -9,3 +9,15 @@ MIN_SUPPORT = 2
 
 df = pd.read_csv(file_location)
 # print(df.head())
+
+# function is used to delete infrequent item/item-sets
+def delete(x, infrequent, axis=1):
+    if x['item' not in list(infrequent['item'])]:
+        return x
+
+# function is used for making combinations of items/item-sets
+def combinations(x, axis=1, k=1):
+    name = x.iloc[0]['Person']
+    combos = list(itertools.combinations(x['item'], k))
+    return pd.DataFrame({'Person': name,
+                         'item': combos})
