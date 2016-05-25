e2ba9dd0aa61d06c4a2450d0d472ff0fe2a1b99f
Varsha Dyavaiah <varsha.lillies@gmail.com>
2016-05-25 13:18:45 +0530

    Support count

--- mba.py
+++ mba.py
@@ -14,10 +14,23 @@ df = pd.read_csv(file_location).drop_duplicates(subset=['Person', 'item'], keep=
 # making the items comma separated
 df['item'] += ','
 
+# support count
+support=pd.value_counts(df['item'].values, sort=False)
+
+#printing support
+print(support)
+
+#printing supoort of a particular item
+print(sup['baby food,'])
+
 # grouping the items based on person or transaction-id
 df = df.groupby('Person').sum()
-
 print(df.head())
+
+# support count
+#support = df.groupby(['item'], as_index=False).count()
+
+#print(support)
 
 
 
